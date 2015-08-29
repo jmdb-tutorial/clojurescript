b4878e15e176d214141a1996ad18d77f24edcb07
Jim Barritt <jim@planet-ix.com>
2015-08-29 16:42:41 +0100

    [jmdb] Step 4 - Adding a repl

--- hello_world/src/hello_world/core.cljs
+++ hello_world/src/hello_world/core.cljs
@@ -1,7 +1,16 @@
-(ns hello-world.core)
+(ns hello-world.core
+  (:require [clojure.browser.repl :as repl]))
+
+(defonce conn
+  (repl/connect "http://localhost:9000/repl")) 
 
 (enable-console-print!)
 
 (println "Hello world!")
-(println "Here is a new statement.")
+
+(defn foo [a b]
+  (+ a b))
+
+
+
 
