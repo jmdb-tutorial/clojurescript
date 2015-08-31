c71268c0dd07b65f7645c02ad0be8c767415495d
Jim Barritt <jim@planet-ix.com>
2015-08-31 22:16:01 +0100

    [jmdb] Adding ability to add a person and clear field to om

--- hello_om/src/hello_om/core.cljs
+++ hello_om/src/hello_om/core.cljs
@@ -2,7 +2,9 @@
     (:require-macros [cljs.core.async.macros :refer [go]])
     (:require[om.core :as om :include-macros true]
              [om.dom :as dom :include-macros true]
-             [cljs.core.async :refer [put! chan <!]]))
+             [cljs.core.async :refer [put! chan <!]]
+             [clojure.data :as data]
+             [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -28,6 +30,27 @@
 (defn display-name [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
 
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (>= c 2) (assoc :middle middle)))))
+
+(defn add-contact [data owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                        .-value
+                        parse-contact)]
+    (when new-contact
+      (om/transact! data :contacts #(conj % new-contact))
+      (om/set-state! owner :text ""))))
+
+(defn handle-change [e owner {:keys [text]}]
+      (om/set-state! owner :text (.. e -target -value)))
+
 (defn contact-view [contact owner]
   (reify
     om/IRenderState
@@ -41,7 +64,8 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:delete (chan)})
+      {:delete (chan)
+       :text ""})
     om/IWillMount
     (will-mount [_]
       (let [delete (om/get-state owner :delete)]
@@ -51,12 +75,16 @@
               (fn [xs] (vec (remove #(= contact %) xs)))) 
             (recur))))))
     om/IRenderState
-    (render-state [this {:keys [delete]}]
+    (render-state [this state]
       (dom/div nil
         (dom/h2 nil "Contact list")
         (apply dom/ul nil
           (om/build-all contact-view (:contacts data)
-            {:init-state {:delete delete}}))))))
+                        {:init-state state}))
+        (dom/div nil
+                 (dom/input #js {:type "text" :ref "new-contact" :value (:text state)
+                                 :onChange #(handle-change % owner state)})
+                 (dom/button #js {:onClick #(add-contact data owner)} "Add contact"))))))
 
 
 
