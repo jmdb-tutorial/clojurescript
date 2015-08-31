034c226f7af4699b6956253e3de2928af9bcfc35
Jim Barritt <jim@planet-ix.com>
2015-08-31 20:46:15 +0100

    [jmdb] More om with stripey lists

--- hello_om/src/hello_om/core.cljs
+++ hello_om/src/hello_om/core.cljs
@@ -10,11 +10,15 @@
 
 (defonce app-state (atom {:list ["Lion", "Zebra", "Buffalo", "Antelope"]}))
 
+(defn stripe [text bgc]
+  (let [st #js {:backgroundColor bgc}]
+    (dom/li #js {:style st} text)))
+
 (om/root
   (fn [data owner]
     (om/component
-     (apply dom/ul nil
-            (map (fn [text] (dom/li nil text)) (:list data)))))
+     (apply dom/ul #js {:className "animals"}
+            (map stripe (:list data) (cycle ["#ff0" "#fff"])))))
   app-state
   {:target (. js/document (getElementById "app0"))})
 
