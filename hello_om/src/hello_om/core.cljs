3958220d7b1d6db492b506018096d0f4977ab6ed
Jim Barritt <jim@planet-ix.com>
2015-08-31 20:35:29 +0100

    [jmdb] Second stage of om tutorial - lists

--- hello_om/src/hello_om/core.cljs
+++ hello_om/src/hello_om/core.cljs
@@ -8,23 +8,16 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello OM!"}))
+(defonce app-state (atom {:list ["Lion", "Zebra", "Buffalo", "Antelope"]}))
 
 (om/root
   (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
+    (om/component
+     (apply dom/ul nil
+            (map (fn [text] (dom/li nil text)) (:list data)))))
   app-state
   {:target (. js/document (getElementById "app0"))})
 
-(om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
-  {:target (. js/document (getElementById "app1"))})
 
 
 (defn on-js-reload []
