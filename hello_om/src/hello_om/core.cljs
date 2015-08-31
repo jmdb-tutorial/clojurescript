741e2f305b68359f3512bb1d90ef3e13cb26ad46
Jim Barritt <jim@planet-ix.com>
2015-08-31 21:10:07 +0100

    [jmdb] Making an OM component

--- hello_om/src/hello_om/core.cljs
+++ hello_om/src/hello_om/core.cljs
@@ -8,19 +8,43 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:list ["Lion", "Zebra", "Buffalo", "Antelope"]}))
+(defonce app-state
+  (atom
+   {:contacts
+    [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+     {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+     {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+     {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+     {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+     {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
-(defn stripe [text bgc]
-  (let [st #js {:backgroundColor bgc}]
-    (dom/li #js {:style st} text)))
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
 
-(om/root
-  (fn [data owner]
-    (om/component
-     (apply dom/ul #js {:className "animals"}
-            (map stripe (:list data) (cycle ["#ff0" "#fff"])))))
-  app-state
-  {:target (. js/document (getElementById "app0"))})
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Contact list")
+               (apply dom/ul nil
+                      (om/build-all contact-view (:contacts data)))))))
+
+
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
 
 
 
